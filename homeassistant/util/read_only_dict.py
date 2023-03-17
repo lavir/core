9677f79c0723b1517a8fc81526b129563f9c9cd8
J. Nick Koston <nick@koston.org>
2023-03-16 21:48:23 -1000

    Cache serialization of shared attributes

--- homeassistant/util/read_only_dict.py
+++ homeassistant/util/read_only_dict.py
@@ -14,6 +14,14 @@ _VT = TypeVar("_VT")
 class ReadOnlyDict(dict[_KT, _VT]):
     """Read only version of dict that is compatible with dict types."""
 
+    def __hash__(self) -> int:  # type: ignore[override]
+        """Return the hash of the dict."""
+        return id(self)
+
+    def __eq__(self, other: object) -> bool:
+        """Return if the dict is equal to another object."""
+        return id(self) == id(other)
+
     __setitem__ = _readonly
     __delitem__ = _readonly
     pop = _readonly
