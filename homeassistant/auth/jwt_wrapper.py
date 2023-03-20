7131c5d3b376c953f1fd90cabd3ecc5e37573d4b
J. Nick Koston <nick@koston.org>
2023-03-20 12:47:13 -1000

    cleanup

---
+++ homeassistant/auth/jwt_wrapper.py
@@ -0,0 +1,80 @@
+"""Provide a wrapper around JWT."""
+from __future__ import annotations
+
+from datetime import timedelta
+from functools import lru_cache
+from typing import Any
+
+from jwt import DecodeError, PyJWS, PyJWT
+
+from homeassistant.util.json import json_loads
+
+
+class _PyJWSWithLoadCache(PyJWS):
+    """PyJWS with a dedicated load implementation."""
+
+    @lru_cache(maxsize=16)
+    def _load(self, jwt: str | bytes) -> tuple[bytes, bytes, dict, bytes]:
+        """Load a JWS."""
+        return super()._load(jwt)
+
+
+_jws = _PyJWSWithLoadCache()
+
+
+class _PyJWTWithVerify(PyJWT):
+    """PyJWT with a dedicated verify implementation."""
+
+    def decode_payload(
+        self,
+        jwt: str,
+        options: dict[str, Any],
+        algorithms: list[str],
+        key: str | None = None,
+    ) -> dict[str, Any]:
+        """Decode a JWT's payload."""
+        try:
+            payload = json_loads(
+                _jws.decode_complete(
+                    jwt=jwt,
+                    key=key or "",
+                    algorithms=algorithms,
+                    options=options,
+                )["payload"]
+            )
+        except ValueError as err:
+            raise DecodeError(f"Invalid payload string: {err}") from err
+        if not isinstance(payload, dict):
+            raise DecodeError("Invalid payload string: must be a json object")
+        return payload
+
+    def verify(
+        self,
+        jwt: str,
+        key: str,
+        algorithms: list[str],
+        issuer: str | None,
+        leeway: int | float | timedelta,
+    ) -> None:
+        """Verify a JWT's signature and claims."""
+        self._validate_claims(  # type: ignore[no-untyped-call]
+            payload=self.decode_payload(
+                jwt=jwt,
+                key=key,
+                options={"verify_signature": True},
+                algorithms=algorithms,
+            ),
+            options={**self.options, "verify_signature": True},
+            issuer=issuer,
+            leeway=leeway,
+        )
+
+
+_jwt = _PyJWTWithVerify()  # type: ignore[no-untyped-call]
+verify = _jwt.verify
+decode_payload = _jwt.decode_payload
+
+__all__ = [
+    "decode_payload",
+    "verify",
+]
