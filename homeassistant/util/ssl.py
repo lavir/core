d68e87f21b57b7dcc7f98a21e414a8d499d3afe3
J. Nick Koston <nick@koston.org>
2023-04-13 06:28:48 -1000

    adjust

--- homeassistant/util/ssl.py
+++ homeassistant/util/ssl.py
@@ -15,10 +15,10 @@ def create_no_verify_ssl_context() -> ssl.SSLContext:
     https://github.com/aio-libs/aiohttp/blob/33953f110e97eecc707e1402daa8d543f38a189b/aiohttp/connector.py#L911
     """
     sslcontext = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-    sslcontext.options |= ssl.OP_NO_SSLv2
-    sslcontext.options |= ssl.OP_NO_SSLv3
     sslcontext.check_hostname = False
     sslcontext.verify_mode = ssl.CERT_NONE
+    # Allow all ciphers rather than only Python 3.10 default
+    sslcontext.set_ciphers("DEFAULT")
     with contextlib.suppress(AttributeError):
         # This only works for OpenSSL >= 1.0.0
         sslcontext.options |= ssl.OP_NO_COMPRESSION
