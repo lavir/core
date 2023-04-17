4d13f4ee1df6da844f8d0f6fcebc9f7840419ac7
J. Nick Koston <nick@koston.org>
2023-04-16 14:08:39 -1000

    fix merge

--- homeassistant/components/onvif/diagnostics.py
+++ homeassistant/components/onvif/diagnostics.py
@@ -27,9 +27,6 @@ async def async_get_config_entry_diagnostics(
         "info": asdict(device.info),
         "capabilities": asdict(device.capabilities),
         "profiles": [asdict(profile) for profile in device.profiles],
-        "webhook_reachable": device.events.webhook_is_working,
-        "webhook_manager_started": device.events.webhook_manager.started,
-        "pullpoint_manager_started": device.events.pullpoint_manager.started,
     }
     data["events"] = {
         "webhook_is_working": device.events.webhook_is_working,
