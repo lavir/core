7db42cd0e0491c5103d306df4f2d6969ca4b9388
J. Nick Koston <nick@koston.org>
2023-04-18 22:30:10 -1000

    tweak

--- homeassistant/components/logbook/queries/entities_and_devices.py
+++ homeassistant/components/logbook/queries/entities_and_devices.py
@@ -6,7 +6,7 @@ from collections.abc import Collection, Iterable
 from sqlalchemy import lambda_stmt, select, union_all
 from sqlalchemy.sql.elements import ColumnElement
 from sqlalchemy.sql.lambdas import StatementLambdaElement
-from sqlalchemy.sql.selectable import CTE, CompoundSelect, Select
+from sqlalchemy.sql.selectable import CompoundSelect, Select
 
 from homeassistant.components.recorder.db_schema import (
     EventData,
@@ -65,14 +65,14 @@ def _apply_entities_devices_context_union(
     json_quoted_entity_ids: list[str],
     json_quoted_device_ids: list[str],
 ) -> CompoundSelect:
-    devices_entities_cte: CTE = _select_entities_device_id_context_ids_sub_query(
+    devices_entities_cte = _select_entities_device_id_context_ids_sub_query(
         start_day,
         end_day,
         event_type_ids,
         states_metadata_ids,
         json_quoted_entity_ids,
         json_quoted_device_ids,
-    ).cte()
+    )
     # We used to optimize this to exclude rows we already in the union with
     # a States.metadata_id.not_in(states_metadata_ids) but that made the
     # query much slower on MySQL, and since we already filter them away
@@ -82,20 +82,14 @@ def _apply_entities_devices_context_union(
         states_select_for_entity_ids(start_day, end_day, states_metadata_ids),
         apply_events_context_hints(
             select_events_context_only()
-            .select_from(devices_entities_cte)
-            .outerjoin(
-                Events, devices_entities_cte.c.context_id_bin == Events.context_id_bin
-            )
             .outerjoin(EventTypes, (Events.event_type_id == EventTypes.event_type_id))
-            .outerjoin(EventData, (Events.data_id == EventData.data_id)),
+            .outerjoin(EventData, (Events.data_id == EventData.data_id))
+            .where(Events.context_id_bin.in_(devices_entities_cte))
         ),
         apply_states_context_hints(
             select_states_context_only()
-            .select_from(devices_entities_cte)
-            .outerjoin(
-                States, devices_entities_cte.c.context_id_bin == States.context_id_bin
-            )
             .outerjoin(StatesMeta, (States.metadata_id == StatesMeta.metadata_id))
+            .where(States.context_id_bin.in_(devices_entities_cte))
         ),
     )
 
