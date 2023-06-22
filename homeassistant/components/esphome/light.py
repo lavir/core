83b8e6fcf1c4cc0d89f851ac60b39be16f69d77f
J. Nick Koston <nick@koston.org>
2023-06-22 03:03:30 +0200

    guard zero

--- homeassistant/components/esphome/light.py
+++ homeassistant/components/esphome/light.py
@@ -104,8 +104,6 @@ def _mired_to_kelvin(mired_temperature: float) -> int:
     This function rounds the converted value instead of flooring the value as
     is done in homeassistant.util.color.color_temperature_mired_to_kelvin().
     """
-    if mired_temperature == 0:
-        return 0
     return round(1000000 / mired_temperature)
 
 
@@ -399,5 +397,6 @@ class EsphomeLight(EsphomeEntity[LightInfo, LightState], LightEntity):
             supported.remove(ColorMode.WHITE)
         self._attr_supported_color_modes = supported
         self._attr_effect_list = static_info.effects
-        self._attr_min_color_temp_kelvin = _mired_to_kelvin(static_info.max_mireds)
-        self._attr_max_color_temp_kelvin = _mired_to_kelvin(static_info.min_mireds)
+        if ColorMode.COLOR_TEMP in supported:
+            self._attr_min_color_temp_kelvin = _mired_to_kelvin(static_info.max_mireds)
+            self._attr_max_color_temp_kelvin = _mired_to_kelvin(static_info.min_mireds)
