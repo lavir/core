146b3ff78d3e148e4f79b41aee5a627478bde487
J. Nick Koston <nick@koston.org>
2023-07-02 11:38:49 -0500

    adjust

--- homeassistant/components/http/__init__.py
+++ homeassistant/components/http/__init__.py
@@ -735,9 +735,18 @@ class FastUrlDispatcher(UrlDispatcher):
             _LOGGER.warning("url %s candidate %s", request.rel_url, url_part)
             if (resource_candidate := resource_index.get(url_part)) is not None:
                 _LOGGER.warning("url %s match candidate %s", request.rel_url, url_part)
-                match_dict, _ = await resource_candidate.resolve(request)
-                if match_dict is not None:
+                if (
+                    match_dict := (await resource_candidate.resolve(request)[0])
+                    is not None
+                ):
                     return match_dict
+        if (index_view_candidate := resource_index.get("/")) is not None:
+            _LOGGER.warning("url %s match candidate %s", request.rel_url, "/")
+            if (
+                match_dict := (await index_view_candidate.resolve(request)[0])
+                is not None
+            ):
+                return match_dict
         # Fallback to the linear search
         _LOGGER.warning("Fallback to linear search for %s", request.rel_url)
         return await super().resolve(request)
