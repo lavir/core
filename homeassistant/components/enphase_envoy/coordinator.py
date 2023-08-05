3a09ff006b292d69fbc8ce7f7d052673f6094260
J. Nick Koston <nick@koston.org>
2023-08-05 09:43:10 -1000

    flow

--- homeassistant/components/enphase_envoy/coordinator.py
+++ homeassistant/components/enphase_envoy/coordinator.py
@@ -51,19 +51,26 @@ class EnphaseUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
         """Set up and authenticate with the envoy."""
         envoy = self.envoy
         await envoy.setup()
-        await envoy.authenticate(username=self.username, password=self.password)
         assert envoy.serial_number is not None
         self.envoy_serial_number = envoy.serial_number
+        await envoy.authenticate(username=self.username, password=self.password)
         self._setup_complete = True
 
     async def _async_update_data(self) -> dict[str, Any]:
         """Fetch all device and sensor data from api."""
         envoy = self.envoy
-        try:
-            if not self._setup_complete:
-                await self._async_setup_and_authenticate()
-            return (await envoy.update()).raw
-        except (EnvoyAuthenticationError, EnvoyAuthenticationRequired) as err:
-            raise ConfigEntryAuthFailed from err
-        except EnvoyError as err:
-            raise UpdateFailed(f"Error communicating with API: {err}") from err
+        for tries in range(2):
+            try:
+                if not self._setup_complete:
+                    await self._async_setup_and_authenticate()
+                return (await envoy.update()).raw
+            except (EnvoyAuthenticationError, EnvoyAuthenticationRequired) as err:
+                if self._setup_complete and tries == 1:
+                    # token likely expired or firmware changed, try to re-authenticate
+                    self._setup_complete = False
+                    continue
+                raise ConfigEntryAuthFailed from err
+            except EnvoyError as err:
+                raise UpdateFailed(f"Error communicating with API: {err}") from err
+
+        raise RuntimeError("Unreachable code in _async_update_data")  # pragma: no cover
